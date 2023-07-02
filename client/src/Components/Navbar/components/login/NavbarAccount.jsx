4c7bed40452d57a21338e338c325fa643584cbd1
Sebastian Ocando <ocandovivas@gmail.com>
2023-07-02 20:34:34 -0300

    navbar login dynamic

--- client/src/Components/Navbar/components/login/NavbarAccount.jsx
+++ client/src/Components/Navbar/components/login/NavbarAccount.jsx
@@ -1,7 +1,14 @@
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../../Context/AuthContext';
 
 export const NavbarAccount = () => {
+    const { user } = useAuth()
+    if (user) return <div className='header__account'>
+        <PersonOutlineOutlinedIcon/>
+        <Link to={'/home'}>Home</Link>
+    </div>
+
     return (
         <div>
             <div  className='header__account'>
